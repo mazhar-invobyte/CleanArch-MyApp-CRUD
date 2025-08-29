8de303d831f0a38ae4218e570fe8b5e3e206ceaf
mazhar-invobyte <mazhar.rehan@invobyte.com>
2025-08-29 17:35:10 +0500

    modify

--- MyApp.Application/DependencyInjection.cs
+++ MyApp.Application/DependencyInjection.cs
@@ -1,10 +1,4 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.Design;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MyApp.Application
 {
--- MyApp.Infrastructure/Repositories/EmployeeRepository.cs
+++ MyApp.Infrastructure/Repositories/EmployeeRepository.cs
@@ -2,11 +2,6 @@
 using MyApp.Core.Entities;
 using MyApp.Core.Interfaces;
 using MyApp.Infrastructure.Data;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MyApp.Infrastructure.Repositories
 {
